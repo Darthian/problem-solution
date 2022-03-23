2570425ed2fcaa15332cd01f4ee5f846c5fd47bd
Ian Sarasty <ian_sarasty@epam.com>
2022-03-23 10:06:37 -0500

    fix finding lowest difference value process

--- src/main/java/com/example/porblemsolution/service/ProblemSolutionServiceImpl.java
+++ src/main/java/com/example/porblemsolution/service/ProblemSolutionServiceImpl.java
@@ -18,20 +18,33 @@ public class ProblemSolutionServiceImpl implements ProblemSolutionService {
     public String solveProblem(String numbersArray) {
         List<Integer> sortedNumbers = getIntegers(numbersArray);
         Collections.sort(sortedNumbers);
-        Integer lowestValue = sortedNumbers.get(1) - sortedNumbers.get(0);
+        final Integer lowestDifferenceValue = getLowestDifferenceValue(sortedNumbers);
         StringBuilder result = new StringBuilder();
         IntStream.range(0, sortedNumbers.size() - 1).forEach(
                 i -> {
-                    if (sortedNumbers.get(i + 1) - sortedNumbers.get(i) == lowestValue) {
+                    if (sortedNumbers.get(i + 1) - sortedNumbers.get(i) == lowestDifferenceValue) {
                         result.append("(" + sortedNumbers.get(i) + ", " + sortedNumbers.get(i + 1) + ")");
                     }
                 });
         return result.toString();
     }
 
+    private Integer getLowestDifferenceValue(List<Integer> sortedNumbers) {
+        Integer differenceValue = 10;
+        for (int i = 0; i < sortedNumbers.size() - 1; i++) {
+            Integer newDifference = sortedNumbers.get(i + 1) - sortedNumbers.get(i);
+            if (newDifference < differenceValue) {
+                differenceValue = newDifference;
+            }
+        }
+
+        final Integer lowestDifferenceValue = differenceValue;
+        return lowestDifferenceValue;
+    }
+
     private List<Integer> getIntegers(String numbersArray) {
         List<Integer> numbers = Arrays.stream(numbersArray
-                        .replaceAll("[^a-zA-Z0-9,]","")
+                        .replaceAll("[^a-zA-Z0-9,]", "")
                         .replaceAll("\\s+", "")
                         .split(","))
                 .map(x -> Integer.valueOf(x).intValue()).collect(Collectors.toList());
